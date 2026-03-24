0d1f89b7928c3d3a45c5ee7574d946d1e00e9594
Samuel <dev.samuel2025@gmail.com>
2026-03-24 15:08:01 -0300

    feat: FINAL - Versao 5 com colunas ordenadas

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ if (dateInput) {
 
 // Form Submission with Google Sheets Integration
 const form = document.getElementById('booking-form');
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwszG95xZdou8jPqecG1-4c9xQIVEllYqKBumCU5Gk4Exn-jezoETdGQd7JMrggxref/exec'; // URL da Versão 3 (Final)
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwUy5Fcb2FHTYYgPqzJ0iiR_ol42M_Ilq_ol27KkRg7Tvc44rqz9wvAitM6TtH7Ujy_/exec'; // URL da Versão 5 (Agendamento Ordenado)
 
 if (form) {
     form.addEventListener('submit', (e) => {
